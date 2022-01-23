f922445e7287bfb8ba3f9d76b6c15bfeb603a55c
André Bonizi <andrebonizi@gmail.com>
2022-01-23 20:38:36 -0300

    Initial Market style

--- src/components/Store.svelte
+++ src/components/Store.svelte
@@ -1,18 +1,65 @@
 <script>
-
+    let products = [
+        { active: true, element: 'Apple', price: 1, level: 1 },
+        { active: true, element: 'Banana', price: 1, level: 1 },
+        { active: true, element: 'Wrench', price: 1, level: 1 },
+        { active: true, element: 'Hammer', price: 1, level: 1 },
+        { active: true, element: 'Knife', price: 1, level: 1 },
+        { active: true, element: 'Shirt', price: 1, level: 1 },
+        { active: true, element: 'Mallet', price:1, level: 1 },
+    ];
 </script>
 
 <div class="container">
-    MARKET
+    <div class="title">
+        Market
+    </div>
+    <hr>
+    <div class="products">
+    {#each products as product}
+        {#if (product && product.active)}
+            <div class="product">
+                <div>
+                    {product.element}
+                </div>
+                <div>
+                    {product.price}
+                </div>
+            </div>    
+        {/if}
+    {/each}
+    </div>
 </div>
 
 <style>
     .container{
         width: 550px;
-        height: 500px;
+        height: 100%;
         border: 5px dotted lightgray;
         padding: 10px;
-
     }
-
+    .products{
+        display: flex;
+        flex-direction: column;
+        text-align: left;
+        background: coral;
+        border-radius: 5px;
+        height: 500px;
+        scroll-behavior: auto;
+    }
+    .product{
+        display: flex;
+        margin: 10px;
+        padding-left: 10px;
+        padding-right: 20px;
+        border-radius: 10px;
+        background: lightgoldenrodyellow;
+        font-family: 'Lobster';
+        justify-content: space-between;
+    }
+    .title{
+        font-weight: bolder;
+        font-size: 2rem;
+        font-family: 'Lobster';
+    }
 </style>
