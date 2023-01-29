5d8ab60dd2fd4dfec9b52113038590ca1fecfa2f
Andre Bonizi <andrebonizi@gmail.com>
2023-01-29 09:00:18 -0300

    include music button component

---
+++ src/components/MusicButton.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    let audio: HTMLAudioElement;
+    let button: HTMLButtonElement;
+
+    function handleMusic(event){
+        event.preventDefault();
+        
+        if (audio.paused){
+            button.style.background = 'linear-gradient(white, green)'
+            audio.play()
+        } else {
+            button.style.background = 'linear-gradient(white, gray)'
+            audio.pause()
+        }
+    }
+
+</script>
+
+<div>
+    <audio 
+        id="bg-music" 
+        src="/music/gangsta_medieval.mp3" 
+        loop
+        controls
+        hidden
+        bind:this={audio}
+    >
+        <track kind="captions" />
+    </audio>	
+    <button 
+        class="music-button"
+        on:click={handleMusic}  
+        id="bg-music-control" 
+        bind:this={button}
+    >
+        🎵
+    </button>
+</div>
+
+<style>
+    .music-button {
+		position: absolute;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: 50%;
+		padding: 15px;
+		bottom: 10px;
+		right: 10px;
+		width: 10px;
+		height: 10px;
+		cursor: pointer;
+		box-shadow: 3px 3px 3px rgb(83, 83, 83);
+		background: linear-gradient(white, gray);
+	}
+</style>
