f2fe96032bab9e88567419d6816606b37c90fe3f
Andre Bonizi <andrebonizi@gmail.com>
2023-01-31 21:19:33 -0300

    user initial commit

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,8 +1,17 @@
 import { collection, doc, getDoc, setDoc } from "firebase/firestore";
 
-export async function getUser(db, user) {
-    if (!user) return null;
+export const player = {
+    life: 10,
+    power: 1,
+    guard: 0,
+    speed: 1,
+    gold: 1,
+    xp: 0,
+    level: 1,
+    name: '',
+}
 
+export async function getUser(db, user) {
     const docRef = doc(db, "users", user.uid);
     const docSnap = await getDoc(docRef);
     
@@ -12,21 +21,55 @@ export async function getUser(db, user) {
         return userDoc;
     } else {
         console.log('New user!', user)
-        return saveUser(db, user);
+        return storeUser(db, user);
     }
 }
 
-export async function saveUser(db, user) {
+export function formatUser(user) {
+    const formatted = {
+        uid: user.uid,
+        name: user.displayName,
+        photoSrc: user.photoURL,
+        player: {
+            life: 10,
+            power: 1,
+            guard: 0,
+            speed: 1,
+            gold: 0,
+            xp: 0,
+            level: 1,
+            inventory: [],
+            equip: {
+                weapon: 0,
+                armor: 0,
+                misc: 0,
+            },
+        }
+    }
+
+    return formatted;
+}
+
+export async function isUserStored(db, user) {
+    const docRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(docRef);
+    
+    return docSnap.exists();
+}
+
+export async function storeUser(db, user) {
+    if (isUserStored(db, user)) return;
+
+    console.log('User is being stored...');
     const usersRef = collection(db, "users");
     await setDoc(
         doc(
             usersRef,
             user.uid
         ), 
-        user
+        formatUser(user)
     );
-    console.log(user.displayName, ' was saved on database.')
-    return user;
+    console.log(user.displayName, ' was saved on database.');
 }
 
 export function getFirstName(user): string {
