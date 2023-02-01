5978b62d4789480a3b0d7dc98870442824f35155
Andre Bonizi <andrebonizi@gmail.com>
2023-01-31 21:17:21 -0300

    add player interfaces

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -2,4 +2,25 @@ export interface User {
     id: string;
     name: string;
     photoSrc: string;
+    player: Player;
+}
+
+export interface Player {
+    level: number;
+    life: number;
+    xp: number;
+    gold: number;
+
+    power: number;
+    guard: number;
+    speed: number;
+
+    inventory: Array<number>;
+    equip: Equip;
+}
+
+export interface Equip {
+    weapon: number;
+    armor: number;
+    misc: number;
 }
