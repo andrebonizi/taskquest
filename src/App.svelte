47e30309684340c16a807eebb195cccae7d051cd
Andre Renato Bonizi <andre.renato.bonizi@g.globo>
2023-02-19 18:11:21 -0300

    fix first click collapse and prettify code

--- src/App.svelte
+++ src/App.svelte
@@ -1,183 +1,186 @@
 <script lang="ts">
-	import type { Config as FirebaseConfig } from './interfaces/firebase';
+  import type { Config as FirebaseConfig } from "./interfaces/firebase";
 
-	import TaskList from './components/TaskList.svelte';
-	import Battle from './components/Battle.svelte';
-	import User from './components/User.svelte';
-	import Inventory from './components/Inventory.svelte';
-	import Status from './components/Status.svelte';
-	import Store from './components/Store.svelte';
-	import { initializeApp } from 'firebase/app';
-	import { login, logout, AUTH_PROVIDER, getFirebaseAuth } from './utils/auth';
-    import { onAuthStateChanged } from 'firebase/auth';
-    import MusicButton from './components/MusicButton.svelte';
-    import { getFirstName, player, storeUser } from './data/user';
-    import { getFirestore } from 'firebase/firestore';
-	import { initialItems } from './data/items';
-	
-	export let firebaseConfig: FirebaseConfig;
+  import TaskList from "./components/TaskList.svelte";
+  import Battle from "./components/Battle.svelte";
+  import User from "./components/User.svelte";
+  import Inventory from "./components/Inventory.svelte";
+  import Status from "./components/Status.svelte";
+  import Store from "./components/Store.svelte";
+  import { initializeApp } from "firebase/app";
+  import { login, logout, AUTH_PROVIDER, getFirebaseAuth } from "./utils/auth";
+  import { onAuthStateChanged } from "firebase/auth";
+  import MusicButton from "./components/MusicButton.svelte";
+  import { getFirstName, player, storeUser } from "./data/user";
+  import { getFirestore } from "firebase/firestore";
+  import { initialItems } from "./data/items";
 
-	const app = initializeApp(firebaseConfig);
-	const auth = getFirebaseAuth(app);
-	const db = getFirestore(app);
-	
-	let battle: boolean;
+  export let firebaseConfig: FirebaseConfig;
 
-	$: loggedUser = null;
-	$: level = 1;
-	$: monster = '';
-	$: items = initialItems;
-	$: hero = player;
+  const app = initializeApp(firebaseConfig);
+  const auth = getFirebaseAuth(app);
+  const db = getFirestore(app);
 
-	onAuthStateChanged(auth, setUer);
+  let battle: boolean;
 
-	function setUer(user) {
-		console.log('User: ', user)
-		loggedUser = user;
-		hero.name = getFirstName(loggedUser);
-		storeUser(db, user);
-	}
+  $: loggedUser = null;
+  $: level = 1;
+  $: monster = "";
+  $: items = initialItems;
+  $: hero = player;
 
-	function startBattle(event) {
-		level = event.detail.level;
-		monster = event.detail.monster;
-		battle = true;
-	}
+  onAuthStateChanged(auth, setUer);
 
-	function handleBattle(event) {
-		hero = event.detail.player;
-		battle = false;
-	}
+  function setUer(user) {
+    console.log("User: ", user);
+    loggedUser = user;
+    hero.name = getFirstName(loggedUser);
+    storeUser(db, user);
+  }
 
-	function playerHit() {
-		alert(`You lost 1 life!`)
-		hero.life-=1;
-	}
+  function startBattle(event) {
+    level = event.detail.level;
+    monster = event.detail.monster;
+    battle = true;
+  }
 
-	function buyItem(event){
-		const newItem = event.detail.product
-		const addToInventory = addItemToInvetory(newItem)
-		if(!addToInventory){
-			return alert('Your inventory is Full!')
-		}
-		hero.gold -= newItem.price
-	}
+  function handleBattle(event) {
+    hero = event.detail.player;
+    battle = false;
+  }
 
-	function addItemToInvetory(newItem){
-		let emptySlot = items.find(item => !item.name)
-		let emptySlotIndex = items.indexOf(emptySlot)
-		if(emptySlotIndex == -1){
-			return (false)
-		}
-		items[emptySlotIndex] = newItem
-		return true
-	}
+  function playerHit() {
+    alert(`You lost 1 life!`);
+    hero.life -= 1;
+  }
 
-	function collapse(event) {
-		const { div } = event.detail;
-		div.style.display = div.style.display !== 'flex' 
-			? 'flex' : 'none';
+  function buyItem(event) {
+    const newItem = event.detail.product;
+    const addToInventory = addItemToInvetory(newItem);
+    if (!addToInventory) {
+      return alert("Your inventory is Full!");
     }
+    hero.gold -= newItem.price;
+  }
+
+  function addItemToInvetory(newItem) {
+    let emptySlot = items.find((item) => !item.name);
+    let emptySlotIndex = items.indexOf(emptySlot);
+    if (emptySlotIndex == -1) {
+      return false;
+    }
+    items[emptySlotIndex] = newItem;
+    return true;
+  }
+
+  function collapse(event) {
+    const { style } = event.detail.div;
+    style.display = style.display === "none" ? "flex" : "none";
+  }
 </script>
 
 <main>
-	{#if battle}
-		<Battle
-			level={level}
-			monster={monster}
-			player={hero}
-			on:endBattle={handleBattle}
-		/>
-	{/if}
+  {#if battle}
+    <Battle {level} {monster} player={hero} on:endBattle={handleBattle} />
+  {/if}
 
-	<div class="header">
-		<div>
-			{#if !loggedUser}
-				<button 
-					on:click={() => {login(auth, AUTH_PROVIDER)}}
-				>
-					Login
-				</button>
-
-			{:else}
-				<p 
-					class="logout-btn"
-					on:click={()=> {logout(auth)}} 
-				>
-					🚪
-				</p>
-			{/if}
-		</div>
-	</div>
-	<div class="container">
-		{#if loggedUser} <User user={loggedUser} hero={hero}/> {/if}
-		<div class="menu">
-			<Status hero={hero} on:change={collapse}/>
-			<Inventory hero={hero} items={items} on:change={collapse}/>
-			<TaskList player={hero} on:startBattle={startBattle} on:playerHit={playerHit}  on:change={collapse}/>
-			<Store gold={hero.gold} on:buyItem={buyItem} on:change={collapse}/>
-		</div>
-		<MusicButton />
-	</div>
+  <div class="header">
+    <div>
+      {#if !loggedUser}
+        <button
+          on:click={() => {
+            login(auth, AUTH_PROVIDER);
+          }}
+        >
+          Login
+        </button>
+      {:else}
+        <p
+          class="logout-btn"
+          on:click={() => {
+            logout(auth);
+          }}
+        >
+          🚪
+        </p>
+      {/if}
+    </div>
+  </div>
+  <div class="container">
+    {#if loggedUser} <User user={loggedUser} {hero} /> {/if}
+    <div class="menu">
+      <Status {hero} on:change={collapse} />
+      <Inventory {hero} {items} on:change={collapse} />
+      <TaskList
+        player={hero}
+        on:startBattle={startBattle}
+        on:playerHit={playerHit}
+        on:change={collapse}
+      />
+      <Store gold={hero.gold} on:buyItem={buyItem} on:change={collapse} />
+    </div>
+    <MusicButton />
+  </div>
 </main>
 
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+  @import url("https://fonts.googleapis.com/css2?family=Lobster&display=swap");
 
-	main {
-		text-align: left;
-		padding: 0;
-		max-width: 100vw;
-		margin: 0;
-		height: 100%;
-		width: 100%;
+  main {
+    text-align: left;
+    padding: 0;
+    max-width: 100vw;
+    margin: 0;
+    height: 100%;
+    width: 100%;
+  }
 
-	}
+  .menu {
+    background: linear-gradient(
+      rgba(165, 42, 42, 0.773),
+      rgba(173, 87, 17, 0.838)
+    );
+    border-radius: 10px;
+    border: 2px outset gray;
+    font-family: "Lobster";
+    width: 100%;
+    padding-bottom: 20px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 20px;
+    height: fit-content;
+  }
 
-	.menu {
-		background: linear-gradient(rgba(165, 42, 42, 0.773), rgba(173, 87, 17, 0.838));
-        border-radius: 10px;
-        border: 2px outset gray;
-        font-family: 'Lobster';
-        width: 100%;
-        padding-bottom: 20px;
-		display: flex;
-		align-items: center;
-		flex-direction: column;
-		gap: 20px;
-		height: fit-content;
-	}
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
+    max-height: 100%;
+  }
 
-	.container{
-		display: flex;
-		flex-direction: column;
-		align-items: flex-start;
-		width: 100%;
-		max-height: 100%;
-	}
+  .header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex: 1;
+  }
 
-	.header{
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		flex: 1;
-	}
+  .logout-btn {
+    cursor: pointer;
+    font-size: 30px;
+    width: min-content;
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
 
-	.logout-btn {
-		cursor: pointer;
-		font-size: 30px;
-		width: min-content;
-		position: absolute;
-		top: 0;
-		right: 0;
-	}
-
-
-	@media screen and (min-width: 800px) {
-		.menu {
-			flex-direction: row;
-			align-items: flex-start;
-			justify-content: space-around;
-		}
-	}
+  @media screen and (min-width: 800px) {
+    .menu {
+      flex-direction: row;
+      align-items: flex-start;
+      justify-content: space-around;
+    }
+  }
 </style>
--- src/components/Status.svelte
+++ src/components/Status.svelte
@@ -1,70 +1,67 @@
 <script>
-    import Attributes from './Attributes.svelte';
-	import { bounceOut } from 'svelte/easing';
-    import { createEventDispatcher, onMount } from 'svelte';
-    import { fly } from 'svelte/transition';
-    import { isMobile } from '../utils/device';
-    
-    export let hero;
-    
-    const dispatch = createEventDispatcher();
-    let container;
-    
-    onMount(() => {
-        console.log('container', container)
-        console.log('device', isMobile())
-        isMobile()? container.style.display = 'none' : 'flex';
-    })
-    
-    function change() {
-        dispatch('change', {
-            div: this.nextSibling.nextSibling
-        })
-    }
+  import Attributes from "./Attributes.svelte";
+  import { bounceOut } from "svelte/easing";
+  import { createEventDispatcher, onMount } from "svelte";
+  import { fly } from "svelte/transition";
+  import { isMobile } from "../utils/device";
+
+  export let hero;
+
+  const dispatch = createEventDispatcher();
+
+  let container;
+
+  onMount(() => {
+    isMobile() ? (container.style.display = "none") : "flex";
+  });
+
+  function change() {
+    dispatch("change", { div: this.nextSibling.nextSibling });
+  }
 </script>
 
 <main>
-    <h2 on:click={change}>🫀 Status</h2>
-    <div class="hero-base" bind:this={container}>
-        Level {hero.level}
-        <div class="level">
-            {#key hero.xp}
-                <progress 
-                    in:fly={{ x: 5,duration: 200, opacity: 1, easing: bounceOut }} 
-                    class="xp-bar" 
-                    value={hero.xp} 
-                    max="100" 
-                />
-                {hero.xp} xp
-            {/key}
-        </div>
-        <Attributes hero={hero}/>
+  <h2 on:click={change}>🫀 Status</h2>
+  <div class="hero-base" bind:this={container}>
+    Level {hero.level}
+    <div class="level">
+      {#key hero.xp}
+        <progress
+          in:fly={{ x: 5, duration: 200, opacity: 1, easing: bounceOut }}
+          class="xp-bar"
+          value={hero.xp}
+          max="100"
+        />
+        {hero.xp} xp
+      {/key}
     </div>
+    <Attributes {hero} />
+  </div>
 </main>
 
 <style>
-    main {
-        width: 270px;
-    }
+  main {
+    width: 270px;
+  }
 
-    .hero-base {
-		display: flex;
-        flex-direction: column;
-        border-radius: 10px;
-        align-items: center;
-        color: black;
-        background-color: rgba(255, 255, 255, 0.421);
-        margin-top: 10px;
-        padding: 20px;
-        gap: 5px;
-    }
+  .hero-base {
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    align-items: center;
+    color: black;
+    background-color: rgba(255, 255, 255, 0.421);
+    margin-top: 10px;
+    padding: 20px;
+    gap: 5px;
+  }
 
-    .level {
-        display: flex;
-        flex-direction: row;
-    }
+  .level {
+    display: flex;
+    flex-direction: row;
+  }
 
-    progress {
-        width: 100px;
-    }
+  progress {
+    width: 100px;
+  }
 </style>
