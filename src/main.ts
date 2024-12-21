dc65a3dbfa4f1dc86a1d7751e6bc83287bf4b978
Andre Bonizi <andrebonizi@gmail.com>
2024-12-21 14:08:50 -0300

    set firebaseconfig as const

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import App from './App.svelte';
 import { getFirebaseConfig } from './firebase/setup';
 
+const firebaseConfig = getFirebaseConfig();
 const app = new App({
   target: document.body,
-  props: {
-    firebaseConfig: getFirebaseConfig(),
-  },
+  props: { firebaseConfig },
 });
 
 export default app;
