02360cc0f64afcfee9153aa588cb9b79e28b169f
Andre Bonizi <andrebonizi@gmail.com>
2024-12-22 22:29:18 -0300

    remove unused code

--- src/data/enemies.ts
+++ src/data/enemies.ts
@@ -1,19 +1,9 @@
 export const enemies = [
-    {level: 1, icon:'🐀', name: 'Rat', taskColor: "rgb(148, 231, 25)"},
-    {level: 2, icon:'🦂', name: 'Scorpion', taskColor: "rgb(11, 117, 9)"},
-    {level: 3, icon:'🐍', name: 'Snake', taskColor: "rgb(197, 164, 43)"},
-    {level: 4, icon:'🐆', name: 'Tiger', taskColor: "rgb(125, 51, 31)"},
-    {level: 5, icon:'🐊', name: 'Crocodille', taskColor: "rgb(219, 1, 1)"},
-    {level: 6, icon:'👹', name: 'Ogre', taskColor: "rgb(255, 0, 0)"},
-    {level: 7, icon:'🐉', name: 'Dragon', taskColor: "rgb(63, 2, 113)"}
+  { level: 1, icon: '🐀', name: 'Rat' },
+  { level: 2, icon: '🦂', name: 'Scorpion' },
+  { level: 3, icon: '🐍', name: 'Snake' },
+  { level: 4, icon: '🐆', name: 'Tiger' },
+  { level: 5, icon: '🐊', name: 'Crocodille' },
+  { level: 6, icon: '👹', name: 'Ogre' },
+  { level: 7, icon: '🐉', name: 'Dragon' },
 ];
-
-const enemiesList = {
-    "rat": {"icon": "🐀", "level": 1},
-    "scorpion": {"icon": "🦂", "level": 2},
-    "snake": {"icon": "🐍", "level": 3},
-    "tiger": {"icon": "🐆", "level": 4},
-    "crocodile": {"icon": "🐊", "level":5},
-    "ogre": {"icon": "👹", "level": 6},
-    "dragon": {"icon": "🐉", "level": 7}
-}
